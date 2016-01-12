403ec9717fa5f479d211bfe97a9ab102a4ce9dd5
Christopher Lau <chrislau85@gmail.com>
2016-01-12 02:46:28 +0000

    Variables exercise 5

---
+++ variables/ex5.rb
@@ -0,0 +1,18 @@
+# x = 0
+# 3.times do
+#   x += 1
+# end
+# puts x
+
+#answer: x = 3
+
+#-----------------
+
+# y = 0
+# 3.times do
+#   y += 1
+#   x = y
+# end
+# puts x
+
+#answer: x throws a variable out of scope error.
