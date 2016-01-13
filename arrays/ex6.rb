30e886ff876b927c62158c792a351676841f9eae
Christopher Lau <chrislau85@gmail.com>
2016-01-13 03:44:52 +0000

    Arrays exercise 6

---
+++ arrays/ex6.rb
@@ -0,0 +1,9 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+
+# TypeError: no implicit conversion of String into Integer
+#   from (irb):2:in `[]='
+#   from (irb):2
+#   from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+# The error was caused by using a string instead of an interger for the array key.
